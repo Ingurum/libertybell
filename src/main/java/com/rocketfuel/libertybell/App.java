e98ddf5af12c0abafa5ddabaa0e2f89c35ca01cf
Ingurum <Stefan@ark>
2016-09-08 21:57:05 +0200

    Added Logging

--- src/main/java/com/rocketfuel/libertybell/App.java
+++ src/main/java/com/rocketfuel/libertybell/App.java
@@ -1,13 +1,20 @@
 package com.rocketfuel.libertybell;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
+public class App {
+	
+	static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+	public static void main( String[] args )
     {
+		LOG.info("Hello {}!", "World");
         System.out.println( "Hello World!" );
     }
 }
